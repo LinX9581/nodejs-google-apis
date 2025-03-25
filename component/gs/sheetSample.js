d23912221f7e5a82fb4b687bfcf8d74472bf5de3
root <root@prod-other-project.c.nownews-227907.internal>
2025-03-25 22:48:02 +0800

    opt sheetSample

--- component/gs/sheetSample.js
+++ component/gs/sheetSample.js
@@ -3,7 +3,7 @@ import * as googleApis from "../../api/googleApis/gsCustom";
 // sheetSample();
 export async function sheetSample() {
   try {
-    await googleApis.updateGsSheet(config.sheetId.test, "test1" + "!A1", [["1asdasd"]]);
+    await googleApis.updateGsSheet(config.sheetId.test, "Sheet1" + "!A1", [["1asdasd"]]);
 
     // sheetId,workTitle , workId must int (random num)
     await googleApis.createGsSheet(config.sheetId.test, "123");
